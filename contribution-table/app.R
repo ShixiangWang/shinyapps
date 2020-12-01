725a79d6058ff388ad2804c1e46a74c6ca0c69cd
Shixiang Wang <wangshx@shanghaitech.edu.cn>
2020-12-01 21:21:33 +0800

    通过将列名移到第一行来支持编辑

--- contribution-table/app.R
+++ contribution-table/app.R
@@ -4,7 +4,18 @@ library(shinyjs)
 library(DataEditR)
 library(contribution)
 
-reset_data <- as.data.frame(matrix(rep("", 100), ncol = 10))
+reset_data <- as.data.frame(matrix(rep("", 49), ncol = 7))
+
+move_colname_to_row <- function(df) {
+  rbind(colnames(df), df)
+}
+
+move_row_to_colname <- function(df) {
+  colheader <- df[1, ]
+  df <- df[-1, ]
+  colnames(df) <- colheader
+  df
+}
 
 ui <- fluidPage(
   theme = "style.css",
@@ -22,12 +33,13 @@ ui <- fluidPage(
     tags$div(
       id = "intro",
       markdown(
-      "This tool generates simple contribution table for credit assignment in a project.\
+        "This tool generates simple contribution table for credit assignment in a project.\
       The first column show the role and the other columns show the people or projects. Read [CRediT](https://casrai.org/credit/) for contributor roles taxonomy.
 
       - For a 3-level contribution table, only 'Minor' and 'Major' are valid, a void cell for no contribution.
       - Please report any suggestion/bug at [here](https://github.com/ShixiangWang/shinyapps/issues). If you are familiar with R, [contribution](https://github.com/openbiox/contribution) package provides more features.\
-      ")
+      "
+      )
     )
   ),
   useShinyjs(),
@@ -57,37 +69,37 @@ ui <- fluidPage(
 server <- function(input,
                    output,
                    session) {
-  data_input1 <- dataInputServer("input1")
+  data_input1 <- dataInputServer("input1", data = c(7, 7))
 
   # Bug: 如果已经有数据，加载文件无法自动显示
   output$data1 <- renderRHandsontable({
     if (!is.null(data_input1())) {
-      rhandsontable(data_input1(), useTypes = FALSE)
+      rhandsontable(move_colname_to_row(data_input1()), colHeaders = NULL, useTypes = FALSE)
     }
   })
 
   observeEvent(input$load, {
     output$data1 <- renderRHandsontable({
       if (!is.null(data_input1())) {
-        rhandsontable(data_input1(), useTypes = FALSE)
+        rhandsontable(move_colname_to_row(data_input1()), colHeaders = NULL, useTypes = FALSE)
       }
     })
   })
 
   observeEvent(input$example, {
     output$data1 <- renderRHandsontable({
-      rhandsontable(contribution::demo, useTypes = FALSE)
+      rhandsontable(move_colname_to_row(contribution::demo), colHeaders = NULL, useTypes = FALSE)
     })
   })
 
   observeEvent(input$clear, {
     output$data1 <- renderRHandsontable({
-      rhandsontable(reset_data, useTypes = FALSE)
+      rhandsontable(reset_data, colHeaders = NULL, useTypes = FALSE)
     })
   })
 
   DF <- reactive({
-    hot_to_r(input$data1)
+    move_row_to_colname(hot_to_r(input$data1))
   })
   observeEvent(input$run, {
     if (any(DF() != "")) {
