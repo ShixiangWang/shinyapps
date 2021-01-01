daf95349c93cb90d720df52376e4c2bbc97add89
Shixiang Wang <wangshx@shanghaitech.edu.cn>
2021-01-02 01:03:20 +0800

    改进逻辑和 ui

--- hiplot-plugin-generator/app.R
+++ hiplot-plugin-generator/app.R
@@ -4,9 +4,11 @@
 # 自动生成 ui 并支持编辑
 # 导出
 
-library(rjson)
+library(data.table)
+library(jsonlite)
 library(dplyr)
 library(shiny)
+library(shinyWidgets)
 library(DTedit) # https://github.com/jbryer/DTedit
 
 ParseString2BoolOrString = function(x) {
@@ -34,13 +36,13 @@ ParseString2FunCall = function(x) {
 
 RtoJSON = function(x) {
   tryCatch(
-    toJSON(x),
+    toJSON(x, null = "null", auto_unbox = TRUE),
     error = function(e) {
       if (class(x) == "name") {
         "" # Report void string to let user modify by themselves
       } else if (class(x) == "call") {
         x = tryCatch(
-          toJSON(eval(x)),
+          toJSON(eval(x), null = "null", auto_unbox = TRUE),
           error = function(e) {
             x = as.character(x)
             idx = !sapply(x, function(y) {
@@ -58,6 +60,17 @@ RtoJSON = function(x) {
     }
   )
 }
+
+# 根据修改后的初始化参数设定生成后端 data.json 和 plot.R 模板文件
+GenerateBackendConfigs = function() {
+
+}
+
+# 根据自定义调整 UI 控件后确定生成 ui.json 文件
+GenerateFrontendConfigs = function() {
+
+}
+
 
 ParseFunArgs = function(pkg_fun) {
   args_list = as.list(args(pkg_fun))
@@ -94,32 +107,84 @@ my.delete.callback <- function(data, row) {
 
 ##### Create the Shiny server
 server <- function(input, output) {
-  mydata = ParseFunArgs(ggpubr::ggboxplot) %>%
-    as.data.frame() %>%
-    tibble::rownames_to_column("id") %>%
-    setNames(c("Parameter", "InitialValue"))
+  observeEvent(input$fun, {
+    pkg_fun = reactive({
+      ic = tryCatch(
+        {
+          class(eval(parse(text = input$fun)))
+        },
+        error = function(err) {
+          showNotification("Function not found!", type = "error")
+          ""
+        }
+      )
+      if (ic != "function") {
+        showNotification("Your input is not a function!", type = "warning")
+        ""
+      } else {
+        input$fun
+      }
+    })
 
-  ##### Create the DTedit object
-  DTedit::dtedit(input, output,
-    name = "args",
-    thedata = mydata,
-    edit.cols = c("Parameter", "InitialValue"),
-    edit.label.cols = c("Parameter", "Initial value (in json format)"),
-    # input.types = c(notes = "textAreaInput"),
-    view.cols = c("Parameter", "InitialValue"),
-    callback.update = my.update.callback,
-    callback.insert = my.insert.callback,
-    callback.delete = my.delete.callback,
-    datatable.options = list(
-      pageLength = 30
+    if (length(pkg_fun()) != 0 & pkg_fun() != "") {
+      mydata = ParseFunArgs(eval(parse(text = pkg_fun()))) %>%
+        as.data.frame() %>%
+        tibble::rownames_to_column("id") %>%
+        setNames(c("Parameter", "InitialValue"))
+    } else {
+      mydata = data.frame(
+        Parameter = character(),
+        InitialValue = character(),
+        stringsAsFactors = FALSE
+      )
+    }
+
+    ##### Create the DTedit object
+    DTedit::dtedit(input, output,
+                   name = "args",
+                   thedata = mydata,
+                   edit.cols = c("Parameter", "InitialValue"),
+                   edit.label.cols = c("Parameter", "Initial value (in json format)"),
+                   # input.types = c(notes = "textAreaInput"),
+                   view.cols = c("Parameter", "InitialValue"),
+                   show.copy = FALSE,
+                   callback.update = my.update.callback,
+                   callback.insert = my.insert.callback,
+                   callback.delete = my.delete.callback,
+                   datatable.options = list(
+                     pageLength = 30
+                   )
     )
-  )
+  })
+
+  ##### 生成 data.json 和 plot.R
+  observeEvent(input$backend, {
+    GenerateFrontendConfigs()
+    # download
+    showNotification("Backend files generated.", type = "success")
+  })
 }
 
 ##### Create the shiny UI
 ui <- fluidPage(
+  h3("Input"),
+  shinyWidgets::searchInput(
+    inputId = "fun",
+    label = "Function to convert",
+    btnSearch = icon("mouse"),
+    btnReset = icon("remove"),
+    placeholder = "ggpubr::ggboxplot",
+    width = "80%"
+  ),
   h3("Editable Function Parameters"),
-  uiOutput("args")
+  column(
+    12,
+    style = "font-size: 75%; width: 75%",
+    align = "center",
+    uiOutput("args")
+  ),
+  br(),
+  actionButton("backend", "Generate backend template files", icon = icon("mouse"))
 )
 
 ##### Start the shiny app
